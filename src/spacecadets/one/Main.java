027f4eebb7fb76e51e594ba9cdad494dcf487374
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-10-15 16:09:55 +0100

    Added imports

--- src/spacecadets/one/Main.java
+++ src/spacecadets/one/Main.java
@@ -2,16 +2,16 @@ package spacecadets.one;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Objects;
 
 
 public class Main {
