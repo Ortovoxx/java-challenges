7cd2192a9eff870e3f78f91ed4231e406e015a84
Ortovoxx <56805259+Ortovoxx@users.noreply.github.com>
2021-10-15 16:07:45 +0100

    Added control flow logic

--- src/spacecadets/one/Main.java
+++ src/spacecadets/one/Main.java
@@ -16,8 +16,41 @@ import java.net.http.HttpResponse;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("Hello!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        while (true) {
+            // Takes in data
+            InputStreamReader input = new InputStreamReader(System.in);
+            BufferedReader reader = new BufferedReader(input);
+
+            // Prompts user for input
+            System.out.println("Please enter your University of Southampton email address: ");
+            String userInput = reader.readLine();
+            if (Objects.equals(userInput, "quit")) break;
+            System.out.println("Fetching required data...");
+
+            // Creates a new Email object
+            Email email = new Email(userInput);
+
+            // Makes a request to the secure site first as everyone is listed there
+            HttpResponse<String> returnedHtml = email.fetchData(Email.fetchDataFrom.AUTHED, email.name);
+
+            if (returnedHtml.body().contains("This page either does not exist or you are not able to view it.")) {
+                System.out.println("This person does not exist");
+                continue;
+            }
+
+            System.out.println("Success");
+
+            var person = new PersonData(returnedHtml.body());
+
+            if (person.visible) {
+                HttpResponse<String> returnedPublicHtml = email.fetchData(Email.fetchDataFrom.PUBLIC, email.name);
+                person.parsePublicHtml(returnedPublicHtml.body());
+            }
+
+            person.storeToJSON();
+        }
     }
 }
 
